1623d214abd82efc954facd954022b61d7eb3920
GoDHubert <tim7840639990414@hotmail.com>
2024-11-19 23:24:46 +0800

    Add dialogue trigger id edit feature

--- scene/common/map_item_view/map_item_view_panel_cntr.gd
+++ scene/common/map_item_view/map_item_view_panel_cntr.gd
@@ -11,6 +11,12 @@ func get_name_label():
 func get_desc_label():
 	return $vbox/desc_label
 
+func get_dialogue_hbox():
+	return $vbox/dialogue_hbox
+
+func get_dialogue_edit() -> SpinBox:
+	return $vbox/dialogue_hbox/dialogue_edit
+
 
 func set_map_item(p_item):
 	map_item = p_item
@@ -26,3 +32,18 @@ func update():
 	nameLabel.text = map_item.get_map_item_name()
 	var descLabel = get_desc_label()
 	descLabel.text = map_item.get_description()
+	
+	var dialogueHbox = get_dialogue_hbox()
+	if map_item is DialogueTrigger:
+		var dialogueEdit = get_dialogue_edit()
+		dialogueEdit.value = map_item.dialogue_id
+		dialogueHbox.show()
+	else:
+		dialogueHbox.hide()
+
+
+func _on_dialogue_edit_changed() -> void:
+	if !map_item is DialogueTrigger:
+		return
+	
+	map_item.dialogue_id = get_dialogue_edit().value
